756f7e34a61a8abf0950e760b36a1112ed7b9d41
Jason Sipula <alupis1@gmail.com>
2013-09-18 12:04:58 -0700

    updated lab02

--- lab02/Lab02.java
+++ lab02/Lab02.java
@@ -11,22 +11,39 @@ import java.util.Scanner;
 public class Lab02 {
 
 	private int year;
+    private final JulianDate jd = new JulianDate();
 
 	public static void main(String[] args) {
 	
-	/* instructor example
-		JulianDate jd = new JulianDate();
-		int date = jd.toJulian(2013,1,1);
-		int dayOfWeek = (date + 1) % 7; // 0 means Sunday, 1 means Monday, etc.
-	*/
-		Lab02 lb = new Lab02();
+	// instructor example
+	//	JulianDate jd = new JulianDate();
+	//        int date = jd.toJulian(2013,1,1);
+	//	int dayOfWeek = (date + 1) % 7; // 0 means Sunday, 1 means Monday, etc.
+	//
+        Lab02 lb = new Lab02();
 		
-		lb.setYear(lb.getUserInputYear());
+		if (args.length == 0) {
+		
+		    lb.setYear(lb.getUserInputYear());
+		
+		} else {
+		
+		    lb.setYear(Integer.parseInt(args[0]));
+		
+		}
+		
 		System.out.print("\n\n");
 		
 		lb.printYear();
-		lb.printMonth(2);
-		lb.printDaysOfWeek();
+
+        // start on 1st month (january)
+        for (int month = 1; month < 12; month++) {
+
+            lb.printMonth(month);
+            lb.printDaysOfWeek();
+            lb.printDayNumbers(month);
+
+        }
 	
 	}
 	
@@ -61,10 +78,10 @@ public class Lab02 {
 		switch(month) {
 		
 			case 1:
-				System.out.printf("\n%13s\n\n", "January");
+				System.out.printf("\n%15s\n\n", "January");
 				break;
 			case 2:
-				System.out.printf("\n%13s\n\n", "February");
+				System.out.printf("\n %15s\n\n", "February");
 				break;
 			case 3:
 				System.out.printf("\n%13s\n\n", "March");
@@ -73,7 +90,7 @@ public class Lab02 {
 				System.out.printf("\n%13s\n\n", "April");
 				break;
 			case 5:
-				System.out.printf("\n%13s\n\n", "May");
+				System.out.printf("\n%12s\n\n", "May");
 				break;
 			case 6:
 				System.out.printf("\n%13s\n\n", "June");
@@ -82,23 +99,29 @@ public class Lab02 {
 				System.out.printf("\n%13s\n\n", "July");
 				break;
 			case 8:
-				System.out.printf("\n%13s\n\n", "August");
+				System.out.printf("\n%14s\n\n", "August");
 				break;
 			case 9:
-				System.out.printf("\n%13s\n\n", "September");
+				System.out.printf("\n%15s\n\n", "September");
 				break;
 			case 10:
-				System.out.printf("\n%13s\n\n", "October");
+				System.out.printf("\n%14s\n\n", "October");
 				break;
 			case 11:
-				System.out.printf("\n%13s\n\n", "November");
+				System.out.printf("\n%15s\n\n", "November");
 				break;
 			case 12:
-				System.out.printf("\n%13s\n\n", "December");
+				System.out.printf("\n%15s\n\n", "December");
 				break;
 		
 		}
 	
 	}
+
+    private void printDayNumbers(int month) {
+
+        jd.toJulian(year, month, 1);
+
+    }
 
 }
