e7a9e4d11b8534cf6968eb98080b1228921f38d4
Jason Sipula <alupis1@gmail.com>
2013-09-18 15:24:58 -0700

    finished lab02

--- lab02/Lab02.java
+++ lab02/Lab02.java
@@ -11,22 +11,40 @@ import java.util.Scanner;
 public class Lab02 {
 
 	private int year;
+	private int numberOfDaysInMonth;
+        private final JulianDate jd = new JulianDate();
 
 	public static void main(String[] args) {
 	
-	/* instructor example
-		JulianDate jd = new JulianDate();
-		int date = jd.toJulian(2013,1,1);
-		int dayOfWeek = (date + 1) % 7; // 0 means Sunday, 1 means Monday, etc.
-	*/
-		Lab02 lb = new Lab02();
+	// instructor example
+	//	JulianDate jd = new JulianDate();
+	//        int date = jd.toJulian(2013,1,1);
+	//	int dayOfWeek = (date + 1) % 7; // 0 means Sunday, 1 means Monday, etc.
+	//
+        Lab02 lb = new Lab02();
 		
-		lb.setYear(lb.getUserInputYear());
+		if (args.length == 0) {
+		
+		    lb.setYear(lb.getUserInputYear());
+		
+		} else {
+		
+		    lb.setYear(Integer.parseInt(args[0]));
+		
+		}
+		
 		System.out.print("\n\n");
 		
 		lb.printYear();
-		lb.printMonth(2);
-		lb.printDaysOfWeek();
+
+        // start on 1st month (january)
+        for (int month = 1; month <= 12; month++) {
+
+            lb.printMonth(month);
+            lb.printDaysOfWeek();
+            lb.printDayNumbers(month);
+
+        }
 	
 	}
 	
@@ -51,8 +69,8 @@ public class Lab02 {
 	
 	private void printDaysOfWeek() {
 	
-		System.out.printf
-		("%3s %2s %s %2s %2s %2s %2s\n", "S", "M", "Tu", "W", "Th", "F", "S");
+		System.out.print
+		("  S  M Tu  W Th  F  S\n");
 	
 	}
 	
@@ -61,44 +79,97 @@ public class Lab02 {
 		switch(month) {
 		
 			case 1:
-				System.out.printf("\n%13s\n\n", "January");
+				System.out.printf("\n%15s\n\n", "January");
+				numberOfDaysInMonth = 31;
 				break;
 			case 2:
-				System.out.printf("\n%13s\n\n", "February");
+				System.out.printf("\n %15s\n\n", "February");
+				numberOfDaysInMonth = getFebruaryLength();
 				break;
 			case 3:
 				System.out.printf("\n%13s\n\n", "March");
+				numberOfDaysInMonth = 31;
 				break;
 			case 4:
 				System.out.printf("\n%13s\n\n", "April");
+				numberOfDaysInMonth = 30;
 				break;
 			case 5:
-				System.out.printf("\n%13s\n\n", "May");
+				System.out.printf("\n%12s\n\n", "May");
+				numberOfDaysInMonth = 31;
 				break;
 			case 6:
 				System.out.printf("\n%13s\n\n", "June");
+				numberOfDaysInMonth = 30;
 				break;
 			case 7:
 				System.out.printf("\n%13s\n\n", "July");
+				numberOfDaysInMonth = 31;
 				break;
 			case 8:
-				System.out.printf("\n%13s\n\n", "August");
+				System.out.printf("\n%14s\n\n", "August");
+				numberOfDaysInMonth = 31;
 				break;
 			case 9:
-				System.out.printf("\n%13s\n\n", "September");
+				System.out.printf("\n%15s\n\n", "September");
+				numberOfDaysInMonth = 30;
 				break;
 			case 10:
-				System.out.printf("\n%13s\n\n", "October");
+				System.out.printf("\n%14s\n\n", "October");
+				numberOfDaysInMonth = 31;
 				break;
 			case 11:
-				System.out.printf("\n%13s\n\n", "November");
+				System.out.printf("\n%15s\n\n", "November");
+				numberOfDaysInMonth = 30;
 				break;
 			case 12:
-				System.out.printf("\n%13s\n\n", "December");
+				System.out.printf("\n%15s\n\n", "December");
+				numberOfDaysInMonth = 31;
 				break;
 		
 		}
 	
 	}
+
+    private void printDayNumbers(int month) {
+
+        int firstDayOfMonth = ((jd.toJulian(year, month, 1) + 1) % 7) + 1;
+
+		if ((firstDayOfMonth - 1) > 0) {
+		
+			System.out.printf("%" + ((firstDayOfMonth - 1) * 3) + "c", ' ');
+		
+		}
+        
+		int currentDay = firstDayOfMonth;
+        for (int j = 1; j <= numberOfDaysInMonth; j++) {
+        
+	        System.out.printf(" %2s", j);
+	  
+			if (currentDay == 7) {
+	        
+				System.out.print("\n");
+				currentDay = 0;
+			
+			}
+        
+			currentDay++;
+		
+        }
+		
+		System.out.print("\n\n");
+
+    }
+        
+    private int getFebruaryLength() {
+        
+		int leapYear = year % 4 == 
+			0 ? 1 : year % 100 == 
+				0 ? 0 : year % 400 == 
+					0 ? 1 : 0;
+	  
+		return (leapYear == 0) ? 29 : 28;
+        
+    }
 
 }
